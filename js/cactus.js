03c2c984b7f85d90d610ea436764189f635909af
al1bex <alibektastan04@gmail.com>
2022-06-18 22:16:46 +0600

    DEL: highscore

--- js/cactus.js
+++ js/cactus.js
@@ -7,7 +7,7 @@ import {
  const SPEED = 0.04;
  const CACTUS_INTERVAL_MIN = 300
  const CACTUS_INTERVAL_MAX = 2000
- const worldElement = document.querySelector("[data-world]")
+ const worldElem = document.querySelector("[data-world]")
 
  
  let nextCactusTime
@@ -46,11 +46,9 @@ import {
    cactus.src = "imgs/cactus.png"
    cactus.classList.add("cactus")
    setCustomProperty(cactus, "--left", 100)
-   worldElement.append(cactus)
+   worldElem.append(cactus)
  }
  
  function randomNumberBetween(min, max) {
    return Math.floor(Math.random() * (max - min + 1) + min)
  }
-
- function updateHighscore()
